59531e6c21dfce8e1a2d50ff8ddece959ec7a637
Rajarshi Parihar <rajarshi.parihar@gmail.com>
2018-02-03 00:01:57 -0800

    First Commit for our Startup.

---
+++ stunners-api/src/main/java/com/stunners/springbootstarter/StunnersApiApp.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.stunners.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Raj
+ *
+ */
+@SpringBootApplication 
+public class StunnersApiApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		SpringApplication.run(StunnersApiApp.class, args);
+		
+	}
+
+}
